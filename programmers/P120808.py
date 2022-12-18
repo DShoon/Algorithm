b7390e40baeb53f7576913f503b017ad44b0fad4
신승훈 <wwiviww@naver.com>
2022-12-18 23:21:07 +0900

    Solved

--- programmers/P120808.py
+++ programmers/P120808.py
@@ -4,10 +4,7 @@ def solution(denum1, num1, denum2, num2):
     num3 = num1 * num2
     
     # 더한 값을 기약분수로 나타내기
-    if denum3 < num3:
-        n = denum3
-    else:
-        n = num3
+    b = min(num3, denum3)
     
     for i in range(1, n+1):
         if (denum3%i)==(num3%i)==0:
